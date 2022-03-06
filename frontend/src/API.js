d380584c8b11ebbb0e2de2996a418550205cf0d4
davidcwh <dcwh12321@gmail.com>
2022-03-06 21:03:53 +0800

    Add env file to frontend

--- frontend/src/API.js
+++ frontend/src/API.js
@@ -1,6 +1,8 @@
+const backendDomain = process.env.REACT_APP_BACKEND_DOMAIN
+
 const get_best_guesses = (words, guess, result) => {
     console.log("making API call: " + guess + ", " + result)
-    return fetch('http://127.0.0.1:5000/api/best-guesses', {
+    return fetch(`${backendDomain}/api/best-guesses`, {
         method: 'POST',
         headers: {
           'Accept': 'application/json',
@@ -10,18 +12,6 @@ const get_best_guesses = (words, guess, result) => {
     })
 }
 
-const test = () => {
-    return fetch('http://127.0.0.1:5000/test', {
-        method: 'POST',
-        headers: {
-          'Accept': 'application/json',
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({"hello": "bye"})
-    })
-}
-
 export default {
     get_best_guesses,
-    test
 }
